b4699550a71a620ed19e1e9143c00dec4dcc586d
Nick Griev <ngriev@gmail.com>
2024-10-04 01:05:16 +0400

    Add gruvbox theme

--- init.lua
+++ init.lua
@@ -842,6 +842,33 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'f4z3r/gruvbox-material.nvim',
+    name = 'gruvbox-material',
+    lazy = false,
+    priority = 1000,
+    opts = {
+      italics = true, -- enable italics in general
+      contrast = 'medium', -- set contrast, can be any of "hard", "medium", "soft"
+      comments = {
+        italics = true, -- enable italic comments
+      },
+      background = {
+        transparent = true, -- set the background to transparent
+      },
+      float = {
+        force_background = false, -- force background on floats even when background.transparent is set
+        background_color = nil, -- set color for float backgrounds. If nil, uses the default color set
+        -- by the color scheme
+      },
+      signs = {
+        highlight = true, -- whether to highlight signs
+      },
+      customize = nil, -- customize the theme in any way you desire, see below what this
+      -- configuration accepts
+    },
+  },
+
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
